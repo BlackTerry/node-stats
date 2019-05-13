110024670f6e11cd3d4f9b6256530fb0797d0a84
FallenGravity <ethanvb55@gmail.com>
2019-05-13 09:44:09 +0200

    Updated

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.IpcProvider("/root/.ether1/geth.ipc",net))
 console.log(web3.isConnected()) // return true if connected
 
-web3.version.getNetwork(callback(error, result){})
+web3.version.getNetwork(callback(error, result)(){})
 
 http.createServer(function (request, response) {
 
