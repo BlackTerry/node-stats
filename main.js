f5b981b8f2f198e95c5c492710b35170c59ef8b1
FallenGravity <ethanvb55@gmail.com>
2019-05-13 09:40:29 +0200

    Updated

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const net  = require('net')
 
 const Web3 = require('web3')
 
+const web3 = new Web3(new Web3.providers.IpcProvider("/root/.ether1/geth.ipc",net))
+console.log(web3.isConnected()) // return true if connected
+
 var version = web3.version.network;
 console.log(version);
 
