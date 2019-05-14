cb374484780e4bd6e4372d0b2099dac966329b75
BlackTerry <thefirstterry@gmail.com>
2019-05-14 19:46:36 +0200

    Updated

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var exphbs  = require('express-handlebars');
 var http = require("http");
 const net  = require('net');
 const Web3 = require('web3');
+
+
 var app = express();
 
 app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
@@ -15,14 +17,30 @@ web3.version.getNetwork((error, result) => {
     console.log('The result is: ' + result)
 });
 
-
 app.get('/', function (req, res, next) {
     web3.version.getNetwork((error, result) => {
         res.render('index',{
-            title:result
+            networkResult:('The result is: ' + result)
         });
     });
 });
+
+app.get('/node', function (req, res, next) {
+    web3.version.getNode((error, result) => {
+        res.render('node',{
+            node:('The node is: ' + result)
+        });
+    });
+});
+
+app.get('/block', function (req, res, next) {
+    web3.eth.getBlockNumber((error, result) => {
+        res.render('block',{
+            block:('The block number is: ' + result)
+        });
+    });
+});
+
 
 app.listen(50000)
 console.log('Server running at http://127.0.0.1:50000/');
